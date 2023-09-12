12cd336228aa2e07d1dc56a60b366fc280471f88
Prabhat Jha <prabhatkumarjha16@gmail.com>
2023-09-12 11:06:48 +0530

    formated the date

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,11 +1,20 @@
 import "./ExpenseItem.css";
 function ExpenseItem(props) {
+  // making the date as human readable.
+  const month = props.date.toLocaleString("en-us", { month: "long" });
+  const day = props.date.toLocaleString("en-us", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{props.expense.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>{props.expense.title}</h2>
-        <div className="expense-item__price">${props.expense.amount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
