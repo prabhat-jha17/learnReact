670cc6076d51788befadfded2279b294923524b9
Prabhat Jha <prabhatkumarjha16@gmail.com>
2023-09-27 22:17:35 +0530

    state concept

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../CommonUI/Card";
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
   const clickHandler = () => {
+    setTitle("Update");
     console.log("clicked!!!");
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>change title</button>
